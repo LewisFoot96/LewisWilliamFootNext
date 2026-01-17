14d1b1f7211a3154c89392044f590d1d34eacd1a
Lewis Foot <lfoot91@hotmail.co.uk>
2026-01-17 15:46:29 +0000

    feat: corrected pipeline to build and deploy from out folder

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/LewisWilliamFootNext",
+  assetPrefix: "/LewisWilliamFootNext/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
