3905df0a7daff1356e37b601ca6e7c672b8e3505
Lewis Foot <lfoot91@hotmail.co.uk>
2026-01-26 19:25:15 +0000

    feat: rename components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import NewComponent from "./NewComponent";
+import BlogPost from "../components/BlogPost";
 
 export default function Home() {
   return (
     <div>
       <h2>Blog Post</h2>
-      <NewComponent />
+      <BlogPost />
     </div>
   );
 }
