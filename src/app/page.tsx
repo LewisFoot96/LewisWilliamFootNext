739e74a862db1cb3c3b80b58485160cd026daa8f
Lewis Foot <lfoot91@hotmail.co.uk>
2026-01-26 19:54:37 +0000

    feat: added MUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import BlogPost from "../components/BlogPost";
+import Button from "@mui/material/Button";
 
 export default function Home() {
   return (
     <div>
       <h2>Blog Post</h2>
       <BlogPost />
+      <Button variant="text">Add item</Button>
     </div>
   );
 }
