cf1af1e001ffb40dee33c9c3b18c048c32cedfde
Lewis Foot <lfoot91@hotmail.co.uk>
2026-01-26 21:21:46 +0000

    feat: added tab navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,44 @@
-import BlogPost from "../components/BlogPost";
-import Button from "@mui/material/Button";
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+import { Tabs, Tab, Box, Typography } from "@mui/material";
+import BlogPost from "../components/BlogPost";
+
+function TabOne() {
   return (
-    <div>
-      <h2>Blog Post</h2>
+    <Box p={2}>
+      <Typography variant="h6">Tab One</Typography>
       <BlogPost />
-      <Button variant="text">Add item</Button>
-    </div>
+    </Box>
+  );
+}
+
+function TabTwo() {
+  return (
+    <Box p={2}>
+      <BlogPost />
+    </Box>
+  );
+}
+
+export default function StaticTabs() {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
+    setActiveTab(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Tabs value={activeTab} onChange={handleChange} centered>
+        <Tab label="Tab One" />
+        <Tab label="Tab Two" />
+      </Tabs>
+
+      <Box sx={{ mt: 2 }}>
+        {activeTab === 0 && <TabOne />}
+        {activeTab === 1 && <TabTwo />}
+      </Box>
+    </Box>
   );
 }
--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,14 +1,22 @@
 import Image from "next/image";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 export default function BlogPost() {
   return (
-    <div>
+    <Box sx={{ width: "100%", maxWidth: 500 }}>
+      <Typography variant="h1" gutterBottom>
+        Great North Run - 2025
+      </Typography>
+      <Typography variant="body1">
+        I took part in the Great North Run.
+      </Typography>
       <Image
         src="images/great-north-run.jpg"
-        width={800}
-        height={600}
+        width={200}
+        height={150}
         alt="Post 1"
       />
-    </div>
+    </Box>
   );
 }
